faaf4783c1861e46699d1a468eae7e3b7b11fd9e
Phạm Tấn Phát <phatdroid94@gmail.com>
2019-05-23 20:58:15 +0700

    a

--- app/src/main/java/khoapham/ptp/phamtanphat/sqlite1903/MonanAdapter.java
+++ app/src/main/java/khoapham/ptp/phamtanphat/sqlite1903/MonanAdapter.java
@@ -7,6 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
 import java.util.List;
 
@@ -20,6 +21,17 @@ public class MonanAdapter extends ArrayAdapter<Monan> {
     public View getView(int position, @Nullable View convertView,  @NonNull ViewGroup parent) {
         LayoutInflater inflater = LayoutInflater.from(getContext());
         convertView = inflater.inflate(R.layout.dong_item_monan,null);
+
+        TextView txtTen = convertView.findViewById(R.id.textviewTen);
+        TextView txtGia = convertView.findViewById(R.id.textviewGia);
+        TextView txtDiachi = convertView.findViewById(R.id.textviewDiachi);
+
+        Monan monan = getItem(position);
+
+        txtTen.setText(monan.getTenmonan());
+        txtGia.setText(monan.getGiamonan() + " đồng");
+        txtGia.setCompoundDrawablesWithIntrinsicBounds(0,0,R.drawable.icon_money,0);
+        txtDiachi.setText(monan.getDiachi());
         return convertView;
     }
 }

8be239b41e93c9dd69d61c78e68b8661e09e5f31
Phạm Tấn Phát <phatdroid94@gmail.com>
2019-05-23 20:00:56 +0700

    a

--- app/src/main/java/khoapham/ptp/phamtanphat/sqlite1903/MainActivity.java
+++ app/src/main/java/khoapham/ptp/phamtanphat/sqlite1903/MainActivity.java
@@ -15,6 +15,13 @@ public class MainActivity extends AppCompatActivity {
 
 //        String createTable = "CREATE TABLE IF NOT EXISTS Monan(Id INTEGER PRIMARY KEY AUTOINCREMENT , Tenmonan VARCHAR , Giamonan INTEGER , Diachi VARCHAR)";
 //        sqLite.QueryData(createTable);
-        String insertData = "INSERT INTO Monan VALUES(null,'Cơm chiên' , 45000 , 'Quận 1')";
+//        String insertData = "INSERT INTO Monan VALUES(null,'Cơm chiên' , 45000 , 'Quận 1')";
+//        String insertData1 = "INSERT INTO Monan VALUES(null,'Hủ tiếu' , 15000 , 'Quận 2')";
+//        String insertData2 = "INSERT INTO Monan VALUES(null,'Canh chua cá lóc' , 35000 , 'Quận 3')";
+//        String insertData3 = "INSERT INTO Monan VALUES(null,'Mì xào hải sản' , 55000 , 'Quận 4')";
+//        sqLite.QueryData(insertData);
+//        sqLite.QueryData(insertData1);
+//        sqLite.QueryData(insertData2);
+//        sqLite.QueryData(insertData3);
     }
 }

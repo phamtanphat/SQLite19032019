b40c9115150d9d8fc018dd4348477fc7608e7d87
Phạm Tấn Phát <phatdroid94@gmail.com>
2019-05-23 21:22:06 +0700

    a

--- app/src/main/java/khoapham/ptp/phamtanphat/sqlite1903/ThemmonanActivity.java
+++ app/src/main/java/khoapham/ptp/phamtanphat/sqlite1903/ThemmonanActivity.java
@@ -2,12 +2,23 @@ package khoapham.ptp.phamtanphat.sqlite1903;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class ThemmonanActivity extends AppCompatActivity {
 
+    EditText edtTen,edtGia,edtDiachi;
+    Button btnHuy,btnXacnhan;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_themmonan);
+
+        edtDiachi = findViewById(R.id.edittextDiachi);
+        edtGia = findViewById(R.id.edittextGiamonan);
+        edtTen = findViewById(R.id.edittextTenmonan);
+        btnHuy = findViewById(R.id.buttonHuy);
+        btnXacnhan = findViewById(R.id.buttonXacnhan);
+
     }
 }
